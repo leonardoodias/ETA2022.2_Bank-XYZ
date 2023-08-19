284c479ed680e9fb17eec4caa00e0fd4f3c36e2e
Beatriz Navarro <32422780+beatriznavarro@users.noreply.github.com>
2023-08-19 13:58:06 -0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,12 @@
 import pytest
 from Pages.AccountPage import AccountPage
+from Pages.AddCustomerPage import AddCustomerPage
 from Pages.CustomerLoginPage import CustomerLoginPage
+from Pages.ManagerPage import ManagerPage
 from Pages.MenuPage import MenuPage
-from Pages.PageObject import PageObject
 
 def pytest_addoption(parser):
-    parser.addoption("--browser", default="chrome", help="Choose browser")
+    parser.addoption("--browser", default="edge", help="Choose browser")
 
 @pytest.fixture
 def select_browser(request):
@@ -29,3 +30,30 @@ def make_deposit(page_login):
     is_deposit = AccountPage(driver=page_login.driver)
     yield is_deposit
     page_login.close()
+
+
+@pytest.fixture
+def setup(select_browser):
+    menu_page = MenuPage(browser=select_browser)
+    yield menu_page
+
+@pytest.fixture()
+def add_customer(setup):
+    setup.open_bank_manager_login()
+    manager_page = ManagerPage(driver=setup.driver)
+    # valida que está na pagína de gerenciamento de cliente
+    assert manager_page.is_url_manager_page(), 'Página de gerenciamento não encontrada!'
+    # Add cliente
+    manager_page.click_add_customer()
+    add_customer = AddCustomerPage(driver=manager_page.driver)
+    add_customer.fill_first_name('Maria')
+    add_customer.fill_last_name('Silva')
+    add_customer.fill_post_code('13174-100')
+    add_customer.click_add_customer()
+    add_customer.check_message_success()
+    yield open_home_page, manager_page
+
+@pytest.fixture()
+def run_all_browser(all_browser):
+    menu_page = MenuPage(browser=all_browser)
+    yield menu_page
